361bffdb55de587fcf9edbb8f9e6673c67db01c0
Tomerkakou <tomerkakou@gmail.com>
2024-06-25 13:13:42 +0300

    F-Add authlayout added

---
+++ fe/src/layouts/auth/index.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { alpha, useTheme } from '@mui/material/styles';
+
+import { bgGradient } from 'theme/css';
+
+import Logo from 'components/logo';
+
+interface AuthLayoutProps {
+    children?: React.ReactNode;
+}
+
+const AuthLayout:React.FC<AuthLayoutProps> = ({children}) => {
+    const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        ...bgGradient({
+          color: alpha(theme.palette.background.default, 0.9),
+          imgUrl: '/assets/background/overlay_4.jpg',
+        }),
+        height: 1,
+      }}
+    >
+      <Logo
+        
+      />
+        {/*sx={{
+          position: 'fixed',
+          top: { xs: 16, md: 24 },
+          left: { xs: 16, md: 24 },
+        }} */}
+      <Stack alignItems="center" justifyContent="center" sx={{ height: 1 }}>
+        <Card
+          sx={{
+            p: 5,
+            width: 1,
+            maxWidth: 420,
+          }}
+        >
+          {children}
+        </Card>
+      </Stack>
+    </Box>
+  )
+}
+
+export default AuthLayout
