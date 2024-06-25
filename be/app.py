8023fb8a6f8ef835321a2c50d9bccabba7ae1da6
Tomerkakou <tomerkakou@gmail.com>
2024-06-25 13:13:16 +0300

    B-Update env db url

--- be/app.py
+++ be/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
 import pymysql
 from dotenv import load_dotenv
 import os
@@ -13,7 +15,7 @@ app = Flask(__name__)
 
 #sqlachemy db
 pymysql.install_as_MySQLdb()
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://learnix:TomerKakou.123@192.168.4.70/learnix"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -21,7 +23,7 @@ db.init_app(app)
 
 @app.route('/')
 def hello():
-    return os.getenv("DB_URL")
+    return str(db.session.execute(text('SELECT * FROM test')).fetchall())
 
 if __name__ == '__main__':
     app.run(debug=True)
