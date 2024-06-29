659fd8b617933268eb42a12e215d9680979c8818
Tomerkakou <tomerkakou@gmail.com>
2024-06-29 15:47:47 +0300

    F -Update files change to tsx

--- fe/src/components/iconify/iconify.tsx
+++ fe/src/components/iconify/iconify.tsx
@@ -1,26 +1,20 @@
-import PropTypes from 'prop-types';
 import { forwardRef } from 'react';
 import { Icon } from '@iconify/react';
 
 import Box from '@mui/material/Box';
+import { Icons } from 'react-toastify';
 
 // ----------------------------------------------------------------------
 
-const Iconify = forwardRef(({ icon, width = 20, sx, ...other }, ref) => (
+const Iconify = forwardRef(({ icon, width = 20, sx, ...other }:any, ref) => (
   <Box
     ref={ref}
     component={Icon}
     className="component-iconify"
     icon={icon}
-    sx={{ width, height: width, ...sx }}
+    sx={{ width, height: width,icon:icon, ...sx }}
     {...other}
   />
 ));
-
-Iconify.propTypes = {
-  icon: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
-  sx: PropTypes.object,
-  width: PropTypes.number,
-};
 
 export default Iconify;
