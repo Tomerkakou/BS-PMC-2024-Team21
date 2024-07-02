57cfd6b214a2f422288019ae5b29d05d4cb8f92b
nironi1998 <117345903+nironi1998@users.noreply.github.com>
2024-07-02 15:49:13 +0300

    Token for user

--- be/routes/auth.py
+++ be/routes/auth.py
@@ -1,11 +1,13 @@
 import base64
+import uuid
 from flask import Blueprint, request, jsonify, abort
 from models.User import User
 from models.User import RoleEnum
+from models.Token import Token, TokenTypeEnum
 from models import db
 from utils.emails.email import sendEmail
 import os
-
+from flask import Flask, redirect, url_for
 auth_blu = Blueprint('auth',__name__)
 
 @auth_blu.post("/sign-up")
@@ -21,14 +23,38 @@ def signUp():
     newUser = User(email = email, password = password, firstName = firstName, lastName = lastName, role= role,avatar=avatar)
     newUser.hashPassword()
     try:
+        
         db.session.add(newUser)
         db.session.commit()
-        
-        sendEmail(email,"Verify your account","verifyAccount",base_url=os.getenv("BASE_URL"),verify_url=os.getenv("BASE_URL"))
+        newToken=Token(Token=str(uuid.uuid4())[0:49],user=newUser.id,token_type=TokenTypeEnum.VerifyEmail)
+        db.session.add(newToken)
+        db.session.commit()
+        url=os.getenv("BASE_URL")+f"/auth/verify-email?token={newToken.Token}"
+        sendEmail(email,"Verify your account","verifyAccount",base_url=os.getenv("FRONT_URL"),verify_url=url)
         return "success", 201
 
     except Exception as e:
+        print(e)
         return "Email already exist!", 400
+
+
+
+
+@auth_blu.get("/verify-email")
+def verifyEmail():
+    token=request.args.get("token")
+    token = Token.query.get(token)
+    if token:
+        user=token.id
+        user.verifiedEmail=True
+        if user.role==RoleEnum.Student:
+            user.active=True
+        db.session.remove(token)
+        db.session.commit()
+    return redirect(os.getenv("FRONT_URL"))
+
+    
+
 
 
     
