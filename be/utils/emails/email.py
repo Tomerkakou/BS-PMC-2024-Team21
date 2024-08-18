cde5524b2f0d4d3e668889a8cce1212470beb3a5
Tomerkakou <tomerkakou@gmail.com>
2024-08-18 14:53:42 +0300

    logo fix

--- be/utils/emails/email.py
+++ be/utils/emails/email.py
@@ -3,7 +3,7 @@ from sendgrid.helpers.mail import Mail
 from be.utils.emails import verifyAccount,resetPassword
 from flask import current_app
 
-LOGO_IMAGE = "https://ibb.co/wNLW38T" 
+LOGO_IMAGE = "https://i.ibb.co/1vfq37z/logo.png" 
 
 templates={
     "verifyAccount":verifyAccount.template,
